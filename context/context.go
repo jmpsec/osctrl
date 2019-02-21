ba5687c01a14e7c87af8bcad3a15a12d8d5bc7b7
Javier Marcos <javier.marcos@gmail.com>
2019-02-20 21:47:48 -0800

    New structure

--- context/context.go
+++ context/context.go
@@ -2,14 +2,10 @@ package context
 
 import (
 	"bytes"
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"io/ioutil"
 	"text/template"
 
 	"github.com/jinzhu/gorm"
-	"github.com/segmentio/ksuid"
 )
 
 const (
@@ -104,33 +100,6 @@ func quickRemoveOneLinerPowershell(context TLSContext) (string, error) {
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};
 iex ((New-Object System.Net.WebClient).DownloadString('https://{{ .TLSHost }}/{{ .Context }}/{{ .SecretPath }}/remove.ps1'))`
 	return quickOneLiner(s, context, "remove.ps1")
-}
-
-// Helper to generate a random string of n characters
-func generateRandomString(n int) string {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
-	if err != nil {
-		return errorRandomString
-	}
-	return base64.URLEncoding.EncodeToString(b)
-}
-
-// Helper to generate a KSUID
-// See https://github.com/segmentio/ksuid for more info about KSUIDs
-func generateKSUID() string {
-	id := ksuid.New()
-	return id.String()
-}
-
-// Helper to read an external file and return contents
-func (context *Context) readExternalFile(path string) string {
-	content, err := ioutil.ReadFile(path)
-	if err != nil {
-		return ""
-	}
-	return string(content)
 }
 
 // Helper to get a quick add script for a context
---
+++ context/util.go
@@ -0,0 +1,36 @@
+package context
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io/ioutil"
+
+	"github.com/segmentio/ksuid"
+)
+
+// Helper to generate a random string of n characters
+func generateRandomString(n int) string {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return errorRandomString
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+// Helper to generate a KSUID
+// See https://github.com/segmentio/ksuid for more info about KSUIDs
+func generateKSUID() string {
+	id := ksuid.New()
+	return id.String()
+}
+
+// Helper to read an external file and return contents
+func (context *Context) readExternalFile(path string) string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+	return string(content)
+}
