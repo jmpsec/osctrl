53fea875a444f10d036110db69112cfc695c4adb
Javier Marcos <javier.marcos@gmail.com>
2020-02-25 13:26:58 -0800

    Fix for git-release

--- tools/git-release.sh
+++ tools/git-release.sh
@@ -14,7 +14,8 @@ fi
 
 BODY="Release $TAG"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-REPO=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+REPO=$(git config --get remote.origin.url | sed 's/.git//g' | awk -F":" '{print $2}' | cut -d'/' -f2)
+OWNER=$(git config --get remote.origin.url | sed 's/.git//g' | awk -F":" '{print $2}' | cut -d'/' -f1)
 
 generate_post_data()
 {
@@ -35,9 +36,12 @@ echo
 
 read -p " -> Github token? " TOKEN
 
+_URL="https://api.github.com/repos/$OWNER/$REPO/releases?access_token=$TOKEN"
+
 echo "[+] Sending POST request to Github API"
+echo "[+] $_URL"
 echo
 
-curl --data "$(generate_post_data)" "https://api.github.com/repos/$REPO/releases?access_token=$TOKEN"
+curl --data "$(generate_post_data)" "$_URL"
 
 echo "[+] Done"
