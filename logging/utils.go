bbf3ace352a9905460e7e624ba5878255cc46202
Javier Marcos <1271349+javuto@users.noreply.github.com>
2022-08-11 03:22:53 +0200

    Prevent logging same data twice

--- logging/utils.go
+++ logging/utils.go
@@ -1,5 +1,7 @@
 package logging
 
+import "github.com/jmpsec/osctrl/backend"
+
 // Helper to remove duplicates from array of strings
 func uniq(duplicated []string) []string {
 	keys := make(map[string]bool)
@@ -11,4 +13,9 @@ func uniq(duplicated []string) []string {
 		}
 	}
 	return result
+}
+
+// Helper to check if two DB configurations are the same
+func sameConfigDB(loggerOne, loggerTwo backend.JSONConfigurationDB) bool {
+	return (loggerOne.Host == loggerTwo.Host) && (loggerOne.Port == loggerTwo.Port) && (loggerOne.Name == loggerTwo.Name)
 }
