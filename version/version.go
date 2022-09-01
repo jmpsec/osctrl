b77bf9f6179508d5db0bcba2d7e0715b95a82b84
Javier Marcos <1271349+javuto@users.noreply.github.com>
2022-09-01 11:45:59 +0200

    Preparing for release v0.3.0

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 const (
 	// OsctrlVersion to have the version for all components
-	OsctrlVersion = "0.2.9"
+	OsctrlVersion = "0.3.0"
 	// OsqueryVersion to have the version for osquery defined
 	OsqueryVersion = "5.4.0"
 )
