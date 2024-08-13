12f2131ff249106383562f835d35f6f1660c25cb
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2024-08-13 17:46:29 +0300

    use OsqueryQueryData from osctrl/logging

--- api/postgres.go
+++ api/postgres.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"gorm.io/gorm"
+	"github.com/jmpsec/osctrl/logging"
 )
 
-// OsqueryQueryData to log query data to database
-type OsqueryQueryData struct {
-	gorm.Model
-	UUID        string `gorm:"index"`
-	Environment string
-	Name        string
-	Data        string
-	Status      int
-}
-
-// APIQueryData to return query results from API
 type APIQueryData map[string]string
 
 // Function to retrieve the query log by name
 func postgresQueryLogs(name string) (APIQueryData, error) {
-	var logs []OsqueryQueryData
+	var logs []logging.OsqueryQueryData
 	data := make(APIQueryData)
 	if err := db.Conn.Where("name = ?", name).Find(&logs).Error; err != nil {
 		return data, err
