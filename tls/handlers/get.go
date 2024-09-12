db11b22e653a1216ee3a438b1c83103f05435965
Javier Marcos <1271349+javuto@users.noreply.github.com>
2024-09-12 21:27:23 +0200

    Refactor of osctrl-tls to organize handlers better

---
+++ tls/handlers/get.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/jmpsec/osctrl/utils"
+)
+
+// RootHandler to be used as health check
+func (h *HandlersTLS) RootHandler(w http.ResponseWriter, r *http.Request) {
+	// Send response
+	utils.HTTPResponse(w, "", http.StatusOK, []byte("💥"))
+}
+
+// HealthHandler for health requests
+func (h *HandlersTLS) HealthHandler(w http.ResponseWriter, r *http.Request) {
+	h.Inc(metricHealthReq)
+	// Send response
+	utils.HTTPResponse(w, "", http.StatusOK, []byte("✅"))
+	h.Inc(metricHealthOK)
+}
+
+// ErrorHandler for error requests
+func (h *HandlersTLS) ErrorHandler(w http.ResponseWriter, r *http.Request) {
+	// Send response
+	utils.HTTPResponse(w, "", http.StatusInternalServerError, []byte("uh oh..."))
+}
