9f6b6c02c063bf6e7593f433251d5a4ed5964da7
Zhuoyuan Liu <zhuoyuan.liu@maersk.com>
2024-12-11 09:49:58 +0100

    Remove isQueryTarget as not be used

--- queries/queries.go
+++ queries/queries.go
@@ -485,26 +485,3 @@ func (q *Queries) UpdateQueryStatus(queryName string, nodeID uint, statusCode in
 	}
 	return nil
 }
-
-// Helper to decide whether if the query targets apply to a give node
-func isQueryTarget(node nodes.OsqueryNode, targets []DistributedQueryTarget) bool {
-	for _, t := range targets {
-		// Check for environment match
-		if t.Type == QueryTargetEnvironment && t.Value == node.Environment {
-			return true
-		}
-		// Check for platform match
-		if t.Type == QueryTargetPlatform && node.Platform == t.Value {
-			return true
-		}
-		// Check for UUID match
-		if t.Type == QueryTargetUUID && node.UUID == t.Value {
-			return true
-		}
-		// Check for localname match
-		if t.Type == QueryTargetLocalname && node.Localname == t.Value {
-			return true
-		}
-	}
-	return false
-}
