fd82c5695c038a415f39fddff3cabb2e9e8d9eb7
Zhuoyuan Liu <zhuoyuan.liu@maersk.com>
2025-02-21 20:08:02 +0100

    move logging to pkg

--- tls/handlers/handlers.go
+++ tls/handlers/handlers.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	"github.com/jmpsec/osctrl/carves"
-	"github.com/jmpsec/osctrl/logging"
 	"github.com/jmpsec/osctrl/metrics"
 	"github.com/jmpsec/osctrl/pkg/environments"
+	"github.com/jmpsec/osctrl/pkg/logging"
 	"github.com/jmpsec/osctrl/pkg/nodes"
 	"github.com/jmpsec/osctrl/queries"
 	"github.com/jmpsec/osctrl/settings"
