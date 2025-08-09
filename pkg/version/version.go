297d21b94e8f73a4e0c375825c7f19122e44c2be
Javier Marcos <1271349+javuto@users.noreply.github.com>
2025-08-09 13:07:22 +0200

    Prepare for release 0.4.5

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 const (
 	// OsctrlVersion to have the version for all components
-	OsctrlVersion = "0.4.4"
+	OsctrlVersion = "0.4.5"
 	// OsqueryVersion to have the version for osquery defined
 	OsqueryVersion = "5.18.1"
 )
--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -11,5 +11,5 @@ func TestOsqueryVersion(t *testing.T) {
 }
 
 func TestOsctrlVersion(t *testing.T) {
-	assert.Equal(t, "0.4.4", OsctrlVersion)
+	assert.Equal(t, "0.4.5", OsctrlVersion)
 }
