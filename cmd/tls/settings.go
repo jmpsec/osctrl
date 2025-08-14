fff2a29a499f1263854fcfd14f36364ce4394464
Javier Marcos <1271349+javuto@users.noreply.github.com>
2025-08-14 23:53:49 +0200

    Fix for --version in osctrl-tls

--- cmd/tls/settings.go
+++ cmd/tls/settings.go
@@ -12,24 +12,24 @@ func loadingSettings(mgr *settings.Settings, cfg config.JSONConfigurationService
 	// Check if service settings for accelerated seconds is ready
 	if !mgr.IsValue(config.ServiceTLS, settings.AcceleratedSeconds, settings.NoEnvironmentID) {
 		if err := mgr.NewIntegerValue(config.ServiceTLS, settings.AcceleratedSeconds, int64(defaultAccelerate), settings.NoEnvironmentID); err != nil {
-			return fmt.Errorf("Failed to add %s to configuration: %w", settings.AcceleratedSeconds, err)
+			return fmt.Errorf("failed to add %s to configuration: %w", settings.AcceleratedSeconds, err)
 		}
 	}
 	// Check if service settings for environments refresh is ready
 	if !mgr.IsValue(config.ServiceTLS, settings.RefreshEnvs, settings.NoEnvironmentID) {
 		if err := mgr.NewIntegerValue(config.ServiceTLS, settings.RefreshEnvs, int64(defaultRefresh), settings.NoEnvironmentID); err != nil {
-			return fmt.Errorf("Failed to add %s to configuration: %w", settings.RefreshEnvs, err)
+			return fmt.Errorf("failed to add %s to configuration: %w", settings.RefreshEnvs, err)
 		}
 	}
 	// Check if service settings for enroll/remove oneliner links is ready
 	if !mgr.IsValue(config.ServiceTLS, settings.OnelinerExpiration, settings.NoEnvironmentID) {
 		if err := mgr.NewBooleanValue(config.ServiceTLS, settings.OnelinerExpiration, defaultOnelinerExpiration, settings.NoEnvironmentID); err != nil {
-			return fmt.Errorf("Failed to add %s to configuration: %w", settings.OnelinerExpiration, err)
+			return fmt.Errorf("failed to add %s to configuration: %w", settings.OnelinerExpiration, err)
 		}
 	}
 	// Write JSON config to settings
 	if err := mgr.SetTLSJSON(cfg, settings.NoEnvironmentID); err != nil {
-		return fmt.Errorf("Failed to add JSON values to configuration: %w", err)
+		return fmt.Errorf("failed to add JSON values to configuration: %w", err)
 	}
 	return nil
 }
