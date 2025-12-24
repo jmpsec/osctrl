28c8c14be9ac0479437e8bfcc24cb4bb95dbdd0d
Javier Marcos <1271349+javuto@users.noreply.github.com>
2025-12-24 08:19:44 +0100

    Prepare for release 0.4.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # osctrl Changelog
 
-## 🔖 Release [0.4.8](https://github.com/jmpsec/osctrl/releases/tag/v0.4.8)
+## 🔖 Release [0.4.9](https://github.com/jmpsec/osctrl/releases/tag/v0.4.9)
 
 ### 🚨 Breaking Changes
 
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 const (
 	// OsctrlVersion to have the version for all components
-	OsctrlVersion = "0.4.8"
+	OsctrlVersion = "0.4.9"
 	// OsqueryVersion to have the version for osquery defined
 	OsqueryVersion = "5.20.0"
 )
