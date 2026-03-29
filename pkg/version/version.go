9c1b8783bd148b45ac79c85d55db0b6385d35771
Javier Marcos <1271349+javuto@users.noreply.github.com>
2026-03-30 00:40:35 +0200

    Retrieving latest release metadata from osctrl.net in osctrl-admin

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,8 +1,47 @@
 package version
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"golang.org/x/mod/semver"
+
+	"github.com/jmpsec/osctrl/pkg/utils"
+)
+
 const (
 	// OsctrlVersion to have the version for all components
 	OsctrlVersion = "0.5.0"
 	// OsqueryVersion to have the version for osquery defined
 	OsqueryVersion = "5.21.0"
+	// VersionDataURL to have the URL to retrieve the latest version for all osctrl components
+	VersionDataURL = "https://stats.osctrl.net/version_data.json"
 )
+
+// VersionData to retrieve the latest version for all osctrl components
+type VersionData struct {
+	LatestRelease    string `json:"latestRelease"`
+	OsqueryVersion   string `json:"osqueryVersion"`
+	SuggestedRelease string `json:"suggestedRelease"`
+	MoreInformation  string `json:"moreInformation"`
+}
+
+// CheckSuggestedRelease to check if the current version is equal or higher than the suggested release
+func CheckSuggestedRelease(suggestedRelease string) bool {
+	return semver.Compare(OsctrlVersion, suggestedRelease) >= 0
+}
+
+func RetrieveVersionData() (*VersionData, error) {
+	status, data, err := utils.SendRequest(http.MethodGet, VersionDataURL, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	if status != http.StatusOK {
+		return nil, err
+	}
+	var versionData VersionData
+	if err := json.Unmarshal(data, &versionData); err != nil {
+		return nil, err
+	}
+	return &versionData, nil
+}
--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -1,15 +1,47 @@
 package version
 
 import (
+	"encoding/json"
+	"net/url"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOsqueryVersion(t *testing.T) {
-	assert.Equal(t, "5.21.0", OsqueryVersion)
+func TestVersionConstantsAreValid(t *testing.T) {
+	semverPattern := regexp.MustCompile(`^\d+\.\d+\.\d+$`)
+
+	assert.Regexp(t, semverPattern, OsctrlVersion)
+	assert.Regexp(t, semverPattern, OsqueryVersion)
 }
 
-func TestOsctrlVersion(t *testing.T) {
-	assert.Equal(t, "0.5.0", OsctrlVersion)
+func TestVersionDataURL(t *testing.T) {
+	parsedURL, err := url.ParseRequestURI(VersionDataURL)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "https", parsedURL.Scheme)
+	assert.Equal(t, "stats.osctrl.net", parsedURL.Host)
+	assert.Equal(t, "/version_data.json", parsedURL.Path)
+}
+
+func TestVersionDataJSONTags(t *testing.T) {
+	data := VersionData{
+		OsqueryVersion:   "5.21.0",
+		LatestRelease:    "0.5.1",
+		SuggestedRelease: "0.5.1",
+		MoreInformation:  "https://docs.example.com/releases/0.5.1",
+	}
+
+	encoded, err := json.Marshal(data)
+	assert.NoError(t, err)
+
+	var decoded map[string]string
+	err = json.Unmarshal(encoded, &decoded)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data.OsqueryVersion, decoded["osqueryVersion"])
+	assert.Equal(t, data.LatestRelease, decoded["latestRelease"])
+	assert.Equal(t, data.SuggestedRelease, decoded["suggestedRelease"])
+	assert.Equal(t, data.MoreInformation, decoded["moreInformation"])
 }
